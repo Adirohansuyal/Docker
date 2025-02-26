e8c8b180f40c514696f570237cddbad23ef4da1f
ohanSuyal <144371692+Adirohansuyal@users.noreply.github.com>
2025-02-26 16:10:50 +0530

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Streamlit App UI
-st.title("📊 Simple Data Visualizer")
+st.title("📊 Simple Data Visuaizer")
 st.write("Upload a CSV file to visualize its data.")
 
 # File uploader
